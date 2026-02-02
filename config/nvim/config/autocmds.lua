d9c5e7b91b876ab996e980034f4cdcf33c355172
torres-rodrigo <torres.costa.rodrigo@gmail.com>
2026-02-01 23:31:43 -0300

    vim autocmd create dirs when saving file

--- config/nvim/config/autocmds.lua
+++ config/nvim/config/autocmds.lua
@@ -4,3 +4,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- Auto create dir when saving a file, Ex: :w not/real/path/file.txt
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+    callback = function(event)
+      if event.match:match("^%w%w+:[\\/][\\/]") then
+        return
+      end
+      local file = vim.uv.fs_realpath(event.match) or event.match
+      vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
+})
