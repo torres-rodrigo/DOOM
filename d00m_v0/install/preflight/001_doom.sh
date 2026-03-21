a97adfb05f6ab75afc4f89c85f90c5b650f39b8d
torres-rodrigo <torres.costa.rodrigo@gmail.com>
2026-03-21 01:12:58 -0300

    script fix

--- d00m_v0/install/preflight/001_doom.sh
+++ d00m_v0/install/preflight/001_doom.sh
@@ -5,6 +5,4 @@ print_step "Initializing pacman"
 run_logged $DOOM_INSTALL/preflight/pacman.sh
 
 print_step "Loading checkpoints"
-run_logged $DOOM_INSTALL/preflight/markers.sh
-
-ECHO "PRE-FLIGHT CHECKS PASSED ✓"
+run_logged $DOOM_INSTALL/preflight/markers.sh   
